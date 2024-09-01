6f11b0b5baa06b2f19d48eb8d930ffea4b0b28b0
vkuasar0 <vvishwakarthik@outlook.com>
2024-09-01 21:32:07 +0530

    Merged events and event details

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -3,29 +3,33 @@ class Event {
   final String title;
   final String date;
   final String status;
-  List<String> participants;
+  List<List<String>> participants;
   List<String> library;
   String? thumbnail;
 
-  Event({
-    required this.id,
-    required this.title,
-    required this.date,
-    required this.status,
-    required this.participants,
-    required this.library,
-    required this.thumbnail
-  });
+  Event(
+      {required this.id,
+      required this.title,
+      required this.date,
+      required this.status,
+      required this.participants,
+      required this.library,
+      required this.thumbnail});
 
   factory Event.fromJson(Map<String, dynamic> json) {
+    List<List<String>> participants = [];
+    if (json['participants'] != null) {
+      for (var participantRow in json['participants']) {
+        participants.add(participantRow.cast<String>());
+      }
+    }
     return Event(
-      id: json['id'].toString(),
-      title: json['title'],
-      date: json['date'],
-      status: json['status'],
-      participants: List<String>.from(json['participants'].map((el)=>el.toString())),
-      library: List<String>.from(json['library'].map((el)=>el.toString())),
-      thumbnail: json['thumbnail']
-    );
+        id: json['id'].toString(),
+        title: json['title'],
+        date: json['date'],
+        status: json['status'],
+        participants: participants,
+        library: List<String>.from(json['library']),
+        thumbnail: json['thumbnail']);
   }
 }
